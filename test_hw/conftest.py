fee4a47e379c091dd36c8bb932f1b506a4679827
Vladimir <vkonovalov74@gmail.com>
2026-02-27 15:35:05 +0500

    Добавил selenoid (без видео)

--- test_hw/conftest.py
+++ test_hw/conftest.py
@@ -7,30 +7,46 @@ from selene import browser
 from utils import attach
 
 
-@pytest.fixture(scope='function', autouse=True)
-def setup_browser(request):
+@pytest.fixture(scope="function", autouse=True)
+def browser_setup():
+
     options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "145.0.7632.117",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": False
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        # command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-        options=options
-    )
 
-    browser.config.driver = driver
+    options.set_capability("selenoid:options", {
+        "enableVNC": True,
+        "enableVideo": False
+    })
 
-    yield browser
+    browser.config.driver_remote_url = "http://selenoid:4444/wd/hub"
+    browser.config.driver_options = options
 
-    attach.add_screenshot(browser)
-    attach.add_html(browser)
-    attach.add_logs(browser)
+    yield
 
     browser.quit()
+
+# @pytest.fixture(scope='function', autouse=True)
+# def setup_browser(request):
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": "145.0.7632.117",
+#         "selenoid:options": {
+#             "enableVNC": True,
+#             "enableVideo": False
+#         }
+#     }
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+#         options=options
+#     )
+#
+#     browser.config.driver = driver
+#
+#     yield browser
+#
+#     attach.add_screenshot(browser)
+#     attach.add_html(browser)
+#     attach.add_logs(browser)
+#
+#     browser.quit()
