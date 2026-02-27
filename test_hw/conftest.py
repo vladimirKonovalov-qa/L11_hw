a39c0b606be778711bc810168c47dc08b85bc41e
Vladimir <vkonovalov74@gmail.com>
2026-02-27 17:42:12 +0500

    Добавил загрузку видео в attach и файл env

--- test_hw/conftest.py
+++ test_hw/conftest.py
@@ -1,10 +1,20 @@
 import pytest
+import os
 
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
-
 from utils import attach
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
@@ -18,7 +28,7 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}.cloud/wd/hub",
         options=options
     )
 
@@ -28,5 +38,6 @@ def setup_browser(request):
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
